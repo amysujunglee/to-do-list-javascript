a2d12b2c3641d8861b51a974c9e9bebd086c298c
amysujunglee <amy.sujunglee@gmail.com>
2020-09-26 20:41:44 -0700

    creating getList function

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const value = document.querySelector('.value');
 const addBtn = document.getElementById('add-btn');
 const lists = document.querySelector('.to-do-list')
-console.log(value, addBtn, lists);
 
 value.addEventListener('keyup', function (e) {
     if (e.keyCode === 13) {
-        e.preventDefault();
-        lists.innerHTML += `<p>${value.value}</p>`;
-        addBtn.click();
+        getList()
     }
 });
+
+function getList() {
+    return lists.innerHTML += `<p>${value.value}</p>`;
+}
